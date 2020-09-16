57089e0b8e9ccfbf6a0372c5f2c08551f21d2781
Anurag Sen <anuragsen21@gmail.com>
2020-09-16 17:01:36 +0530

    16092020 hotel details models/hotel

---
+++ templates/main/js/script.js
@@ -0,0 +1,60 @@
+/******slider*****/
+$(document).ready(function () {
+
+	$('#slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: false ,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+
+
+
+});
+
+/******slider*****/
+
+/******slider*****/
+
+$(document).ready(function () {
+
+	$('#testimonial_slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+
+
+
+});
+
+/******slider*****/
