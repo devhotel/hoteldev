5471ba8cc21f254ba035746c82ea138fc5728d21
Anurag Sen <anuragsen21@gmail.com>
2020-09-16 11:11:56 +0530

    16092020*

--- templates/main/models/home.php
+++ templates/main/models/home.php
@@ -6,29 +6,21 @@
 $stylesheets[] = array('file' => DOCBASE . 'js/plugins/royalslider/royalslider.css', 'media' => 'all');
 $stylesheets[] = array('file' => DOCBASE . 'js/plugins/royalslider/skins/minimal-white/rs-minimal-white.css', 'media' => 'all');
 $javascripts[] = DOCBASE . 'js/plugins/royalslider/jquery.royalslider.min.js';
-
 $javascripts[] = DOCBASE . 'js/plugins/live-search/jquery.liveSearch.js';
-
 require(getFromTemplate('common/header.php', false));
-
 $slide_id = 0;
 $result_slide_file = $db->prepare('SELECT * FROM pm_slide_file WHERE id_item = :slide_id AND checked = 1 AND lang = ' . DEFAULT_LANG . ' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1');
 $result_slide_file->bindParam('slide_id', $slide_id);
-
 $result_slide = $db->query('SELECT * FROM pm_slide WHERE id_page = ' . $page_id . ' AND checked = 1 AND lang = ' . LANG_ID . ' ORDER BY rank', PDO::FETCH_ASSOC);
 if ($result_slide !== false) {
     $nb_slides = $db->last_row_count();
     if ($nb_slides > 0) { ?>
-
         <div id="search-home-wrapper">
             <div id="search-home" class="container">
                 <?php include(getFromTemplate('common/search-home.php', false)); ?>
             </div>
         </div>
-
-
         <section id="sliderContainer">
-
             <div class="royalSlider rsMinW fullSized clearfix">
                 <?php
                 foreach ($result_slide as $i => $row) {
@@ -36,22 +28,16 @@ if ($result_slide !== false) {
                     $slide_legend = $row['legend'];
                     $url_video = $row['url'];
                     $id_page = $row['id_page'];
-
                     $result_slide_file->execute();
-
                     if ($result_slide_file !== false && $db->last_row_count() == 1) {
                         $row = $result_slide_file->fetch();
-
                         $file_id = $row['id'];
                         $filename = $row['file'];
                         $label = $row['label'];
-
                         $realpath = SYSBASE . 'medias/slide/big/' . $file_id . '/' . $filename;
                         $thumbpath = DOCBASE . 'medias/slide/small/' . $file_id . '/' . $filename;
                         $zoompath = DOCBASE . 'medias/slide/big/' . $file_id . '/' . $filename;
-
                         if (is_file($realpath)) { ?>
-
                             <div class="rsContent">
                                 <img class="rsImg" src="<?php echo $zoompath; ?>" alt="" <?php if ($url_video != '') echo ' data-rsVideo="' . $url_video . '"'; ?>>
                                 <?php
@@ -74,7 +60,6 @@ if ($result_slide !== false) {
 <?php
 $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER BY id DESC ')->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 <section class="top_testimonial">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -102,43 +87,34 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
             $result_hotel = $db->query('SELECT * FROM pm_hotel WHERE lang = ' . LANG_ID . ' AND checked = 1 AND home = 1 ORDER BY rank');
             if ($result_hotel !== false) {
                 $nb_hotels = $db->last_row_count();
-
                 $hotel_id = 0;
-
                 $result_hotel_file = $db->prepare('SELECT * FROM pm_hotel_file WHERE id_item = :hotel_id AND checked = 1 AND lang = ' . DEFAULT_LANG . ' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1');
                 $result_hotel_file->bindParam(':hotel_id', $hotel_id);
-
-                $result_rate = $db->prepare('SELECT MIN(price) as min_price FROM pm_rate WHERE id_hotel = :hotel_id');
+                //$result_rate = $db->prepare('SELECT MIN(price) as min_price FROM pm_rate WHERE id_hotel = :hotel_id');
+                $result_rate = $db->prepare('SELECT MIN(price) as min_price FROM pm_room WHERE checked = 1 AND id_hotel = :hotel_id');
                 $result_rate->bindParam(':hotel_id', $hotel_id);
-
                 foreach ($result_hotel as $i => $row) {
                     $hotel_id = $row['id'];
                     $hotel_title = $row['title'];
                     $hotel_subtitle = $row['subtitle'];
-
                     $hotel_alias = DOCBASE . $pages[9]['alias'] . '/' . text_format($row['alias']);
-
                     $min_price = 0;
                     if ($result_rate->execute() !== false && $db->last_row_count() > 0) {
                         $row = $result_rate->fetch();
                         $price = $row['min_price'];
                         if ($price > 0) $min_price = $price;
                     } ?>
-
                     <article class="col-sm-4 mb20" itemscope itemtype="http://schema.org/LodgingBusiness">
                         <a itemprop="url" href="<?php echo $hotel_alias; ?>" class="moreLink">
                             <?php
                             if ($result_hotel_file->execute() !== false && $db->last_row_count() == 1) {
                                 $row = $result_hotel_file->fetch(PDO::FETCH_ASSOC);
-
                                 $file_id = $row['id'];
                                 $filename = $row['file'];
                                 $label = $row['label'];
-
                                 $realpath = SYSBASE . 'medias/hotel/small/' . $file_id . '/' . $filename;
                                 $thumbpath = DOCBASE . 'medias/hotel/small/' . $file_id . '/' . $filename;
                                 $zoompath = DOCBASE . 'medias/hotel/big/' . $file_id . '/' . $filename;
-
                                 if (is_file($realpath)) {
                                     $s = getimagesize($realpath); ?>
                                     <figure class="more-link">
@@ -146,8 +122,6 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                                             <img alt="<?php echo $label; ?>" data-src="<?php echo $thumbpath; ?>" itemprop="photo" width="<?php echo $s[0]; ?>" height="<?php echo $s[1]; ?>">
                                         </div>
                                         <div class="more-content">
-                                            <!--  <h3 itemprop="name"><?php //echo $hotel_title; 
-                                                                        ?></h3> -->
                                             <?php
                                             if ($min_price > 0) { ?>
                                                 <div class="more-descr">
@@ -199,23 +173,18 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                             $activity_alias = $row['title'];
                             $activity_subtitle = $row['subtitle'];
                             $min_price = $row['price'];
-
                             $activity_alias = DOCBASE . $sys_pages['activities']['alias'] . '/' . text_format($row['alias']); ?>
-
                             <article class="col-sm-3 mb20" itemscope itemtype="http://schema.org/LodgingBusiness">
                                 <a itemprop="url" href="<?php echo $activity_alias; ?>" class="moreLink">
                                     <?php
                                     if ($result_activity_file->execute() !== false && $db->last_row_count() > 0) {
                                         $row = $result_activity_file->fetch(PDO::FETCH_ASSOC);
-
                                         $file_id = $row['id'];
                                         $filename = $row['file'];
                                         $label = $row['label'];
-
                                         $realpath = SYSBASE . 'medias/activity/small/' . $file_id . '/' . $filename;
                                         $thumbpath = DOCBASE . 'medias/activity/small/' . $file_id . '/' . $filename;
                                         $zoompath = DOCBASE . 'medias/activity/big/' . $file_id . '/' . $filename;
-
                                         if (is_file($realpath)) {
                                             $s = getimagesize($realpath); ?>
                                             <figure class="more-link">
@@ -247,7 +216,6 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
     $result_destination = $db->query('SELECT * FROM pm_destination WHERE lang = ' . LANG_ID . ' AND checked = 1 AND home = 1 ORDER BY rank');
     if ($result_destination !== false) {
         $nb_destinations = $db->last_row_count();
-
         if ($nb_destinations > 0) { ?>
             <div class="dest_section">
                 <div class="container">
@@ -255,10 +223,8 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                         <h2 class="text-center mt5 mb10"><?php echo $texts['TOP_DESTINATIONS']; ?></h2>
                         <?php
                         $destination_id = 0;
-
                         $result_destination_file = $db->prepare('SELECT * FROM pm_destination_file WHERE id_item = :destination_id AND checked = 1 AND lang = ' . DEFAULT_LANG . ' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1');
                         $result_destination_file->bindParam(':destination_id', $destination_id);
-
                         $result_rate = $db->prepare('
                         SELECT MIN(ra.price) as min_price
                         FROM pm_rate as ra, pm_hotel as h, pm_destination as d
@@ -266,35 +232,28 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                             AND id_destination = d.id
                             AND id_destination = :destination_id');
                         $result_rate->bindParam(':destination_id', $destination_id);
-
                         foreach ($result_destination as $i => $row) {
                             $destination_id = $row['id'];
                             $destination_name = $row['name'];
                             $destination_subtitle = $row['subtitle'];
-
                             $destination_alias = DOCBASE . $sys_pages['booking']['alias'] . '/' . text_format($row['alias']);
-
                             $min_price = 0;
                             if ($result_rate->execute() !== false && $db->last_row_count() > 0) {
                                 $row = $result_rate->fetch();
                                 $price = $row['min_price'];
                                 if ($price > 0) $min_price = $price;
                             } ?>
-
                             <article class="col-sm-4 mb20" itemscope itemtype="http://schema.org/LodgingBusiness">
                                 <a itemprop="url" href="<?php echo $destination_alias; ?>" class="moreLink">
                                     <?php
                                     if ($result_destination_file->execute() !== false && $db->last_row_count() == 1) {
                                         $row = $result_destination_file->fetch(PDO::FETCH_ASSOC);
-
                                         $file_id = $row['id'];
                                         $filename = $row['file'];
                                         $label = $row['label'];
-
                                         $realpath = SYSBASE . 'medias/destination/small/' . $file_id . '/' . $filename;
                                         $thumbpath = DOCBASE . 'medias/destination/small/' . $file_id . '/' . $filename;
                                         $zoompath = DOCBASE . 'medias/destination/big/' . $file_id . '/' . $filename;
-
                                         if (is_file($realpath)) {
                                             $s = getimagesize($realpath); ?>
                                             <figure class="more-link">
@@ -339,7 +298,6 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
     $result_article = $db->query('SELECT * FROM pm_article WHERE (id_page = ' . $page_id . ' OR home = 1) AND checked = 1 AND (publish_date IS NULL || publish_date <= ' . time() . ') AND (unpublish_date IS NULL || unpublish_date > ' . time() . ') AND lang = ' . LANG_ID . ' ORDER BY rank');
     if ($result_article !== false) {
         $nb_articles = $db->last_row_count();
-
         if ($nb_articles > 0) { ?>
             <div class="container mt10">
                 <div class="row">
@@ -355,11 +313,8 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                             $char_limit = ($i == 0) ? 1200 : 500;
                             $article_text = strtrunc(strip_tags($row['text'], '<p><br>'), $char_limit, true, '');
                             $article_page = $row['id_page'];
-
                             if (isset($pages[$article_page])) {
-
                                 $article_alias = DOCBASE . $pages[$article_page]['alias'] . '/' . text_format($article_alias); ?>
-
                                 <article id="article-<?php echo $article_id; ?>" class="col-sm-<?php echo ($i == 0) ? 12 : 4; ?> " itemscope itemtype="http://schema.org/Article">
                                     <div class="<?php echo ($i == 0) ? 'row' : ''; ?> ">
                                         <div class="<?php echo ($i != 0) ? 'art_small' : ''; ?>">
@@ -368,15 +323,12 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                                                     <?php
                                                     if ($result_article_file->execute() !== false && $db->last_row_count() == 1) {
                                                         $row = $result_article_file->fetch(PDO::FETCH_ASSOC);
-
                                                         $file_id = $row['id'];
                                                         $filename = $row['file'];
                                                         $label = $row['label'];
-
                                                         $realpath = SYSBASE . 'medias/article/big/' . $file_id . '/' . $filename;
                                                         $thumbpath = DOCBASE . 'medias/article/big/' . $file_id . '/' . $filename;
                                                         $zoompath = DOCBASE . 'medias/article/big/' . $file_id . '/' . $filename;
-
                                                         if (is_file($realpath)) {
                                                             $s = getimagesize($realpath); ?>
                                                             <figure class="more-link">
@@ -415,72 +367,8 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
     <?php
         }
     } ?>
-    <?php
-    /*
-    $v_slide_id = 0;
-        $v_result_slide_file = $db->prepare('SELECT * FROM pm_vendors_file WHERE id_item = :v_slide_id AND checked = 1 AND lang = '.DEFAULT_LANG.' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1');
-        $v_result_slide_file->bindParam('v_slide_id', $v_slide_id);
-
-        $v_result_slide = $db->query('SELECT * FROM pm_vendors WHERE id_page = '.$page_id.' AND checked = 1 AND lang = '.LANG_ID.' ORDER BY rank', PDO::FETCH_ASSOC);
-        if($v_result_slide !== false){
-    ?>
-    <div class="container">
-   <section class="customer-logos slider">
-       <?php 
-        foreach($v_result_slide as $i => $row){
-                    $v_slide_id = $row['id'];
-                    $slide_legend = $row['legend'];
-                    $url_v = $row['url'];
-                    $id_page = $row['id_page'];
-                    
-                    $v_result_slide_file->execute();
-                    
-                    if($v_result_slide_file !== false && $db->last_row_count() == 1){
-                        $row = $v_result_slide_file->fetch();
-                        $file_id = $row['id'];
-                        $filename = $row['file'];
-                        $label = $row['label'];
-                        
-                        $realpath = SYSBASE.'medias/vendors/big/'.$file_id.'/'.$filename;
-                        $thumbpath = DOCBASE.'medias/vendors/small/'.$file_id.'/'.$filename;
-                        $zoompath = DOCBASE.'medias/vendors/big/'.$file_id.'/'.$filename;
-                        ?>
-      <div class="slide"><a href="<?php echo $url_v; ?>" target="_blank"><img src="<?php echo $zoompath; ?>"></a></div>
-      <?php } }  ?>
-   </section>
-   <script>
-       $(document).ready(function(){
-        $('.customer-logos').slick({
-        loop:true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 1
-            }
-        }]
-    });
-});
-   </script>
-       
-    <?php }*/ ?>
 </section>
 </div>
-
-
-
-
 <!-- Modal -->
 <div id="covidModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
@@ -493,13 +381,13 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                 <h2>COVID-19 Safety Measures at HMS</h2>
                 <ul>
                     <li>Thermal screening at the entry both for staff and guests</li>
-    <li>Sanitization at the entry and as & when required</li>
-    <li>Separate dustbin for all used masks and gloves, cleared in a sterile manner</li>
-    <li>Extra masks and gloves are available for those who need</li>
-    <li>Our staff wears appropriate safety kits</li>
-    <li>Each and every room gets sanitized after guests check out</li>
-    <li>Sanitization of entire property is being done at regular intervals</li>
-    <li>Our Staff keep safe distance while serving or providing any services to the guests</li>
+                    <li>Sanitization at the entry and as & when required</li>
+                    <li>Separate dustbin for all used masks and gloves, cleared in a sterile manner</li>
+                    <li>Extra masks and gloves are available for those who need</li>
+                    <li>Our staff wears appropriate safety kits</li>
+                    <li>Each and every room gets sanitized after guests check out</li>
+                    <li>Sanitization of entire property is being done at regular intervals</li>
+                    <li>Our Staff keep safe distance while serving or providing any services to the guests</li>
                 </ul>
                 <p>Maintaining all safety and sanitization norms, we are ready to serve you again with same passion as before.</p>
                 <p><b>Team HMS</b></p>
