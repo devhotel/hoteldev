fd9f87a574acbff7bdd896114b4352c80df77819
Anurag Sen <anuragsen21@gmail.com>
2020-09-16 13:54:39 +0530

    16092020 home page

--- templates/main/models/home.php
+++ templates/main/models/home.php
@@ -99,14 +99,11 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                     $hotel_subtitle = $row['subtitle'];
                     $hotel_alias = DOCBASE . $pages[9]['alias'] . '/' . text_format($row['alias']);
                     $min_price = 0;
-
-                    echo date('Y-m-d H:i:s', strtotime(date('Y-m-d'))); die;
-
-                    $newMinDiscPriceQ   = $db->query('SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
-                    $newMinPriceQ       = $db->query('SELECT MIN(new_price) as new_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
+                    $newMinDiscPriceQ   = $db->query('SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d', strtotime(date('Y-m-d'))))->fetch(PDO::FETCH_ASSOC);
+                    $newMinPriceQ       = $db->query('SELECT MIN(new_price) as new_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d', strtotime(date('Y-m-d'))))->fetch(PDO::FETCH_ASSOC);
                     $newMinPrice        = (!empty($newMinPriceQ['new_price'])) ? $newMinPriceQ['new_price'] : '0';
                     $newMinDiscPrice    = (!empty($newMinDiscPriceQ['new_price'])) ? $newMinDiscPriceQ['new_price'] : '0';
-                    echo 'SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d');
+                    echo 'SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d', strtotime(date('Y-m-d')));
                     print_r($newMinPrice); 
                     print_r($newMinDiscPrice); 
                     die;
