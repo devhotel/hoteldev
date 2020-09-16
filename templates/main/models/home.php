1aa1f61e7a79821bfd343ae424bc9261da588c29
Anurag Sen <anuragsen21@gmail.com>
2020-09-16 13:40:26 +0530

    16092020 home page

--- templates/main/models/home.php
+++ templates/main/models/home.php
@@ -95,9 +95,10 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                 $result_rate->bindParam(':hotel_id', $hotel_id);
 
 
-                $newMinPrice = $db->query('SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
-                $newMinDiscPrice = $db->query('SELECT MIN(new_price) as new_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
-
+                $newMinDiscPriceQ   = $db->query('SELECT MIN(new_disc_price) as new_disc_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
+                $newMinPriceQ       = $db->query('SELECT MIN(new_price) as new_price FROM pm_room_new_stock_rate WHERE id_hotel = ' . $hotel_id . ' AND date = ' . date('Y-m-d'))->fetch(PDO::FETCH_ASSOC);
+                $newMinPrice        = (!empty($newMinPriceQ['new_price'])) ? $newMinPriceQ['new_price'] : '0';
+                $newMinDiscPrice    = (!empty($newMinDiscPriceQ['new_price'])) ? $newMinDiscPriceQ['new_price'] : '0';
                 print_r($newMinPrice); 
                 print_r($newMinDiscPrice); 
                 die;
@@ -112,7 +113,9 @@ $testimonial = $db->query('SELECT * FROM pm_testimonial where status = 1 ORDER B
                     if ($result_rate->execute() !== false && $db->last_row_count() > 0) {
                         $row = $result_rate->fetch();
                         $price = $row['min_price'];
-                        if ($price > 0) $min_price = $price;
+                        if ($price > 0):
+                            $min_price = ($newMinPrice != 0 && $newMinDiscPrice != 0) ? (($newMinDiscPrice <= $newMinPrice) ? (($newMinDiscPrice < $price) ? $newMinDiscPrice : $price) : (($newMinPrice < $price) ? $newMinPrice : $price)) : $price;
+                        endif;
                     } ?>
                     <article class="col-sm-4 mb20" itemscope itemtype="http://schema.org/LodgingBusiness">
                         <a itemprop="url" href="<?php echo $hotel_alias; ?>" class="moreLink">
