c7393d9e8b2c275f4de9cd427868981f4d5bf5e4
Anurag Sen <anuragsen21@gmail.com>
2020-09-16 15:03:11 +0530

    16092020 search-sode

--- templates/main/common/search-side.php
+++ templates/main/common/search-side.php
@@ -6,18 +6,14 @@ $result_adult->execute();
 $result_children->execute();
 $max_adults_search = $result_adult->fetch()['max_adults_qty'];
 $max_children_search = $result_children->fetch()['max_child_qty'];
-
 if (!isset($_SESSION['destination_id'])) $_SESSION['destination_id'] = 0;
 if (!isset($destination_name)) $destination_name = '';
-
 if (!isset($_SESSION['num_adults']))
     $_SESSION['num_adults'] = (isset($_SESSION['book']['adults'])) ? $_SESSION['book']['adults'] : 1;
 if (!isset($_SESSION['num_children']))
     $_SESSION['num_children'] = (isset($_SESSION['book']['children'])) ? $_SESSION['book']['children'] : 0;
-
 $from_date = (isset($_SESSION['from_date'])) ? $_SESSION['from_date'] : '';
 $to_date = (isset($_SESSION['to_date'])) ? $_SESSION['to_date'] : '';
-
 /****** Searched destinatation *****/
 if (isset($_REQUEST['destination_id']) && is_numeric($_REQUEST['destination_id'])) {
     $_SESSION['destination_id'] = $_REQUEST['destination_id'];
@@ -29,7 +25,6 @@ if (isset($_REQUEST['destination_id']) && is_numeric($_REQUEST['destination_id']
     $_SESSION['destination_id'] = 0;
     $destination_name = '';
 }
-
 /****** Searched accommodation *****/
 if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_id'])) {
     $_SESSION['accommodation_id'] = $_REQUEST['accommodation_id'];
@@ -42,7 +37,6 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
     $_SESSION['accommodation_id'] = 0;
     $accommodation_name = '';
 }
-
 ?>
 <div class="side_search">
     <form id="form_search" action="<?php echo DOCBASE . $sys_pages['booking']['alias']; ?>" method="get" class="booking-search">
@@ -51,16 +45,8 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
             <input type="hidden" name="hotel_id" value="<?php echo $hotel_id; ?>">
         <?php
         } ?>
-        <input type="hidden" name="destination_id" id="destination_id" value="<?php if ($_SESSION['destination_id']) {
-                                                                                    echo $_SESSION['destination_id'];
-                                                                                } else {
-                                                                                    echo 1;
-                                                                                } ?>">
-        <input type="hidden" name="accommodation_id" id="accommodation_id" value="<?php if ($_SESSION['accommodation_id']) {
-                                                                                        echo $_SESSION['accommodation_id'];
-                                                                                    } else {
-                                                                                        echo 0;
-                                                                                    } ?>">
+        <input type="hidden" name="destination_id" id="destination_id" value="<?=($_SESSION['destination_id']) ? $_SESSION['destination_id'] : 1?>">
+        <input type="hidden" name="accommodation_id" id="accommodation_id" value="<?=($_SESSION['accommodation_id']) ? $_SESSION['accommodation_id'] : 0?>">
         <input type="hidden" name="from_date" value="<?php echo $from_date; ?>">
         <input type="hidden" name="to_date" value="<?php echo $to_date; ?>">
         <input type="hidden" name="num_adults" value="<?php echo $_SESSION['num_adults']; ?>">
@@ -71,7 +57,6 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
         if ($page['page_model'] == 'booking') { ?>
             <div class="row mb5 mt10">
                 <?php if (isset($_SESSION['price_range'])) {
-                    //var_dump($_POST['price_range']);
                     $price_range = explode('-', $_SESSION['price_range']);
                     $pmin = $price_range[0];
                     $pmax = $price_range[1];
@@ -97,54 +82,52 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
                         if (!isset($price_min) || is_null($price_min)) $price_min = 0;
                         if (!isset($price_max) || is_null($price_max)) $price_max = $max_price; ?>
                         <div class="boxed" ">
-                        <div class=" form-group gp-aside">
-                            <label class="control-label" for="hotel_class"><?php echo $texts['YOUR_BUDGET']; ?></label>
-                            <div class="col-sm-12">
-                                <!--<div class="nouislider-wrapper">
-                                    <div class="nouislider" data-min="0" data-max="<?php echo number_format(ceil($max_price) * CURRENCY_RATE, 0, '.', ''); ?>" data-start="<?php echo '[' . number_format(floor($price_min) * CURRENCY_RATE, 0, '.', '') . ',' . number_format(ceil($price_max) * CURRENCY_RATE, 0, '.', '') . ']'; ?>" data-step="10" data-direction="<?php echo RTL_DIR; ?>" data-input="price_range"></div>
-                                   <span> <?php echo $texts['PRICE'] . ' / ' . $texts['NIGHT']; ?> : <?php echo CURRENCY_SIGN; ?></span> <input type="text" name="price_range" class="slider-target" id="price_range" value="" readonly="readonly" size="15">
-                                </div> -->
-
-                                <?php
-                                $rang = array('0' => '1000', '1001' => '2000', '2001' => '4000', '4001' => '9999');
-                                foreach ($rang as $price => $mprice) {
-                                    $selected = (isset($pmax) && $pmax == $mprice) ? ' checked="checked"' : ''; ?>
-                                    <span class="checkbox"><input type="checkbox" class="check_price" name="price_range" value="<?php echo $price; ?>-<?php echo $mprice; ?>" <?php echo $selected ?>><?php echo formatPrice($price * CURRENCY_RATE); ?>-<?php echo formatPrice($mprice * CURRENCY_RATE); ?></span>
-                                <?php
-                                }
-                                ?>
+                            <div class=" form-group gp-aside">
+                                <label class="control-label" for="hotel_class"><?php echo $texts['YOUR_BUDGET']; ?></label>
+                                <div class="col-sm-12">
+                                    <!--<div class="nouislider-wrapper">
+                                        <div class="nouislider" data-min="0" data-max="<?php echo number_format(ceil($max_price) * CURRENCY_RATE, 0, '.', ''); ?>" data-start="<?php echo '[' . number_format(floor($price_min) * CURRENCY_RATE, 0, '.', '') . ',' . number_format(ceil($price_max) * CURRENCY_RATE, 0, '.', '') . ']'; ?>" data-step="10" data-direction="<?php echo RTL_DIR; ?>" data-input="price_range"></div>
+                                    <span> <?php echo $texts['PRICE'] . ' / ' . $texts['NIGHT']; ?> : <?php echo CURRENCY_SIGN; ?></span> <input type="text" name="price_range" class="slider-target" id="price_range" value="" readonly="readonly" size="15">
+                                    </div> -->
+                                    <?php
+                                    $rang = array('0' => '1000', '1001' => '2000', '2001' => '4000', '4001' => '9999');
+                                    foreach ($rang as $price => $mprice) {
+                                        $selected = (isset($pmax) && $pmax == $mprice) ? ' checked="checked"' : ''; ?>
+                                        <span class="checkbox"><input type="checkbox" class="check_price" name="price_range" value="<?php echo $price; ?>-<?php echo $mprice; ?>" <?php echo $selected ?>><?php echo formatPrice($price * CURRENCY_RATE); ?>-<?php echo formatPrice($mprice * CURRENCY_RATE); ?></span>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
                             </div>
                         </div>
-            </div>
-    <?php
+                    <?php
                     }
                 }
                 if (!isset($class_min) || is_null($class_min)) $class_min = 0;
                 if (!isset($class_max) || is_null($class_max)) $class_max = 5; ?>
-    <div class="boxed">
-        <div class="form-group gp-aside">
-            <label class="control-label" for="hotel_class"><?php echo $texts['HOTEL_CLASS']; ?></label>
-            <div class="col-sm-12">
-                <!-- <div class="nouislider-wrapper">
-                            <div class="nouislider" data-min="0" data-max="5" data-start="<?php echo '[' . $class_min . ',' . $class_max . ']'; ?>" data-step="1" data-direction="<?php echo RTL_DIR; ?>" data-input="class_range"></div>
-                            <span><?php echo $texts['STARS']; ?> :</span> <input type="text" name="class_range" class="slider-target" id="class_range" value="" readonly="readonly" size="5">
-                        </div> -->
-                <?php
-                for ($str = 1; $str <= 5; $str++) {
-                    $selected = (isset($class_max) && $class_max == $str) ? ' checked="checked"' : '';
-                ?>
-                    <span class="checkbox"><input type="checkbox" class="check_star" name="class_range" value="<?php echo $str; ?>-<?php echo $str; ?>" <?php echo $selected ?>><input type="hidden" class="rating" value="<?php echo $str; ?>" data-rtl="<?php echo (RTL_DIR) ? true : false; ?>" data-size="xs" readonly="true" data-show-clear="false" data-show-caption="false"></span>
-
-                <?php }
-                ?>
+        <div class="boxed">
+            <div class="form-group gp-aside">
+                <label class="control-label" for="hotel_class"><?php echo $texts['HOTEL_CLASS']; ?></label>
+                <div class="col-sm-12">
+                    <!-- <div class="nouislider-wrapper">
+                                <div class="nouislider" data-min="0" data-max="5" data-start="<?php echo '[' . $class_min . ',' . $class_max . ']'; ?>" data-step="1" data-direction="<?php echo RTL_DIR; ?>" data-input="class_range"></div>
+                                <span><?php echo $texts['STARS']; ?> :</span> <input type="text" name="class_range" class="slider-target" id="class_range" value="" readonly="readonly" size="5">
+                            </div> -->
+                    <?php
+                    for ($str = 1; $str <= 5; $str++) {
+                        $selected = (isset($class_max) && $class_max == $str) ? ' checked="checked"' : '';
+                    ?>
+                        <span class="checkbox"><input type="checkbox" class="check_star" name="class_range" value="<?php echo $str; ?>-<?php echo $str; ?>" <?php echo $selected ?>><input type="hidden" class="rating" value="<?php echo $str; ?>" data-rtl="<?php echo (RTL_DIR) ? true : false; ?>" data-size="xs" readonly="true" data-show-clear="false" data-show-caption="false"></span>
+                    <?php }
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
             $nb_search_destinations = 0;
             $result_search_destination = $db->query('SELECT * FROM pm_destination WHERE checked = 1 AND lang = ' . LANG_ID);
             if ($result_search_destination !== false) {
-    ?>
+        ?>
         <div class="boxed">
             <div class="form-group gp-aside">
                 <label class="control-label" for="hotel_class"><?php echo $texts['DESTINATION']; ?></label>
@@ -160,16 +143,15 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
                         $selected = (isset($_SESSION['destination_id']) && $_SESSION['destination_id'] == $row['id']) ? ' checked="checked"' : '';
                         echo '<span class="checkbox"><input type="checkbox" class="check_desti" name="destination_id" value="' . $row['id'] . '"' . $selected . '>' . $row['name'] . '</span>';
                     } ?>
-
                 <?php
                 } ?>
             </div>
         </div>
     <?php } ?>
     <?php
-            $nb_search_accommodation = 0;
-            $result_search_accommodation = $db->query('SELECT * FROM pm_accommodation WHERE checked = 1 AND lang = ' . LANG_ID);
-            if ($result_search_accommodation !== false) {
+    $nb_search_accommodation = 0;
+    $result_search_accommodation = $db->query('SELECT * FROM pm_accommodation WHERE checked = 1 AND lang = ' . LANG_ID);
+    if ($result_search_accommodation !== false) {
     ?>
         <div class="boxed">
             <div class="form-group gp-aside">
@@ -194,10 +176,9 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
 </div>
 <?php
         } ?>
-
 <input type="hidden" name="check_availabilities" value="">
 </form>
-<form id="form_reset" action="<?php echo DOCBASE . $sys_pages['booking']['alias']; ?>" method="get" class="booking-search">
+<form id="form_reset" action="<?php echo DOCBASE . $sys_pages['booking']['alias']; ?>" method="post" class="booking-search">
     <input type="hidden" name="destination_id" id="destination_id" value="0">
     <input type="hidden" name="accommodation_id" id="accommodation_id" value="0">
     <input type="hidden" name="from_date" value="<?php echo $from_date; ?>">
@@ -212,5 +193,4 @@ if (isset($_REQUEST['accommodation_id']) && is_numeric($_REQUEST['accommodation_
     <button class="btn btn-block btn-primary" type="button" name="check_availabilities" onclick="booking_search()">GO</button>
     <button class="btn btn-block btn-primary" type="button" name="check_availabilities" onclick="booking_reset()">Reset</button>
 </div>
-
 </div>
