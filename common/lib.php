60f240753bdbafd7fa8bf8fce537aaa494e0d925
Anurag Sen <anuragsen21@gmail.com>
2021-05-10 15:07:23 +0530

    a

--- common/lib.php
+++ common/lib.php
@@ -1430,7 +1430,7 @@ function formatPrice($price, $currency = CURRENCY_SIGN)
  */
 function getFromTemplate($path, $docbase = true)
 {
-    echo $path; die;
+    echo $path;
     $base = $docbase ? DOCBASE : SYSBASE;
     $default_path = 'templates/default/' . $path;
     if (TEMPLATE == 'default')
